385c20e57d3f99143b5d2b3e74d5dbb6653f5a29
Shane T Wirkes <swirke2@shane-wirkess-macbook.local>
2017-10-03 20:22:11 +0300

    Initial Commit

---
+++ nscale/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nscale
+//
+//  Created by Shane T Wirkes on 10/3/17.
+//  Copyright © 2017 Shane Wirkes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
